929b543a0b584b503cf4641317052bb7e2f0be00
nikita <nikita1999@gmail.com>
2025-11-26 15:22:41 +0530

    added config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Configuration file that loads environment variables
 const CONFIG = {
-    WEATHER_API_KEY: Add_YOUR_API_KEY_HERE,
+    WEATHER_API_KEY: '494985f9106bee9b64a897e2ebd440cd',
     BASE_URL: 'https://api.openweathermap.org/data/2.5'
 };
